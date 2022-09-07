fd9d6598b775bda2f5fd32f06b8fe5a35bfdde82
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-09-07 12:29:53 -0400

    complete controllers/index.js and auth.js for viewing content when user is logged in

---
+++ utils/auth.js
@@ -0,0 +1,13 @@
+const withAuth = (req, res, next) => {
+    // if user not logged in, redirect user to home page/login page
+    // directly from `/list` and `/restaurants/:id` routes
+    if (!req.session.logedIn) {
+        res.redirect('/login');
+    } else {
+        // if user logged in, execute route function to allow user to view restaurants or list
+        // call next() if user is authenticated
+        next();
+    }
+};
+
+module.exports = withAuth;
