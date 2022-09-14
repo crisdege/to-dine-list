2a0edeb0ff0b56439f746d5dfa48dcea0fa70300
Cristina Dege <cris.dege0813@gmail.com>
2022-09-14 13:44:58 -0400

    homepage and responsiveness complete

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,73 +1,73 @@
-const router = require('express').Router();
-const { Restaurant, Cuisine, User } = require('../models');
+const router = require("express").Router();
+const { Restaurant, Cuisine, User } = require("../models");
 // middleware import
 // const withAuth = require('../utils/auth');
 
 // GET restaurants list for homepage
-router.get('/', async (req, res) => {
-    if (!req.session.loggedIn) {
-        // render homepage without user authentication
-        res.render('homepage', { loggedIn: false });
+router.get("/", async (req, res) => {
+  if (!req.session.loggedIn) {
+    // render homepage without user authentication
+    res.render("homepage", { loggedIn: false });
+  } else {
+    // render homepage with user's restaurants list
+    try {
+      // get restaurants by user id
+      const dbRestaurantData = await Restaurant.findAll({
+        // get user id from session id
+        where: { user_id: req.session.user_id },
+        include: [
+          {
+            model: Restaurant,
+            attributes: ["id", "name", "cuisine_id", "rating"],
+          },
+          {
+            model: Cuisine,
+            attributes: ["id", "name", "cuisine_image"],
+          },
+        ],
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+
+    if (!dbRestaurantData) {
+      // if the user has no restaurants, go to add restaurant page
+      res.render("add-restaurant", { loggedIn: req.session.loggedIn });
     } else {
-        // render homepage with user's restaurants list
-        try {
-            // get restaurants by user id
-            const dbRestaurantData = await Restaurant.findAll({
-                // get user id from session id
-                where: { user_id: req.session.user_id },
-                include: [
-                    {
-                        model: Restaurant,
-                        attributes: ['id', 'name', 'cuisine_id', 'rating']
-                    },
-                    {
-                        model: Cuisine,
-                        attributes: ['id', 'name', 'cuisine_image']
-                    }
-                ],
-            });
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        };
+      // if the user has restaurants, clean them up for use by handlebars
+      const restaurantData = dbRestaurantData.map((restaurant) =>
+        restaurantData.get({ plain: true })
+      );
 
-        if (!dbRestaurantData) {
-            // if the user has no restaurants, go to add restaurant page
-            res.render('add-restaurant', { loggedIn: req.session.loggedIn });
+      // add boolean values for displaying ratings
+      let restaurants = restaurantData.map((restaurant) => {
+        restaurant.rating1 = false;
+        restaurant.rating2 = false;
+        restaurant.rating3 = false;
+
+        if (restaurant.rating === 0) {
+          restaurant.ratingCheck = false;
         } else {
-            // if the user has restaurants, clean them up for use by handlebars
-            const restaurantData = dbRestaurantData.map((restaurant) =>
-                restaurantData.get({ plain: true })
-            );
+          restaurant.ratingCheck = true;
 
-            // add boolean values for displaying ratings
-            let restaurants = restaurantData.map((restaurant) => {
-                restaurant.rating1 = false;
-                restaurant.rating2 = false;
-                restaurant.rating3 = false;
+          if (restaurant.rating === 1) {
+            restaurant.rating1 = true;
+          } else if (restaurant.rating === 2) {
+            restaurant.rating2 = true;
+          } else {
+            restaurant.rating3 = true;
+          }
+        }
+      });
 
-                if (restaurant.rating === 0) {
-                    restaurant.ratingCheck = false;
-                } else {
-                    restaurant.ratingCheck = true;
-
-                    if (restaurant.rating === 1) {
-                        restaurant.rating1 = true;
-                    } else if (restaurant.rating === 2) {
-                        restaurant.rating2 = true;
-                    } else {
-                        restaurant.rating3 = true;
-                    };
-                };
-            })
-
-            // render homepage with user's restaurants
-            res.render('homepage', {
-                restaurants,
-                loggedIn: req.session.loggedIn
-            });
-        };
-    };
+      // render homepage with user's restaurants
+      res.render("homepage", {
+        restaurants,
+        loggedIn: req.session.loggedIn,
+      });
+    }
+  }
 });
 
 // GET one restaurant
--- public/js/login.js
+++ public/js/login.js
@@ -1,52 +1,56 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#').value.trim();  
-    const password = document.querySelector('#').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      //check if the response status
-      if (response.ok) {
-        console.log('success');
-        document.location.reload();
-      } else {
-        alert(response.statusText);
-      }
-    }  
-  }
-  
-  async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#').value.trim();
-    const password = document.querySelector('#').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/api/restaurants');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    //check if the response status
+    if (response.ok) {
+      console.log("success");
+      document.location.reload();
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document.querySelector('#signup-btn').addEventListener('submit', signupFormHandler);
-  document.querySelector('#login-btn').addEventListener('submit', loginFormHandler);
+}
+
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/api/restaurants");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".signup-btn")
+  .addEventListener("submit", signupFormHandler);
+document
+  .querySelector(".login-btn")
+  .addEventListener("submit", loginFormHandler);
