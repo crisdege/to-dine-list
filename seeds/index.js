516baf8a50dc79dce6844a0d48a5ecf49c312131
Tien Ngo <ngoquoctien24196@gmail.com>
2022-09-14 00:10:27 -0400

    edited seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,18 @@
 const sequelize = require("../config/connection");
 const seedRestaurants = require("./restaurantsData");
+const seedCuisines = require("./cuisineData");
+const seedUser = require("./UserData");
+
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedRestaurants();
+  await seedUser();
+  seedCuisines();
+  seedRestaurants();
+        
+        
+
 
   process.exit(0);
 };
